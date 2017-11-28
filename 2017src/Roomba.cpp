4104185fbd7dd385af7b1c883ee9eaac48b0dfff
Myles Brophy <mbrophy@usc.edu>
2017-11-27 19:39:04 -0800

    correct Roomba math

--- 2017src/Roomba.cpp
+++ 2017src/Roomba.cpp
@@ -6,6 +6,5 @@ Roomba::Roomba(double x, double y, double z, double dir) : Robot(x, y, z, dir) {
 }
 
 std::tuple<double, double, double, long> simulate(double time){
-	return std::make_tuple(0.0, 0.0, 0.0, 0);
-	//return std::make_tuple(x+(speed*cos(theta)*time), y+(speed*sin(theta)*time), z, theta);
+	return std::make_tuple(x+(speed*cos(theta)*time), y+(speed*sin(theta)*time), z, theta);
 }
